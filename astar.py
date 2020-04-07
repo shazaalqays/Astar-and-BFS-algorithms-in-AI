7514b9cf68d96f535e6a50bf8c7deaaed86a8d1a
shazaalqays <37289506+shazaalqays@users.noreply.github.com>
2020-04-07 19:40:36 +0300

    Add files via upload

---
+++ astar.py
@@ -0,0 +1,121 @@
+from PIL import Image, ImageDraw
+from numpy import asarray
+import numpy as np
+
+class Point():
+    """A Point class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(resized, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given image"""
+
+    # Create start and end node
+    start_point = Point(None, start)
+    start_point.g = start_point.h = start_point.f = 0
+    end_point = Point(None, end)
+    end_point.g = end_point.h = end_point.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start point
+    open_list.append(start_point)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current point
+        current_point = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_point.f:
+                current_point = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_point)
+
+        # Found the goal
+        if current_point == end_point:
+            path = []
+            current = current_point
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get point position
+            point_position = (current_point.position[0] + new_position[0], current_point.position[1] + new_position[1])
+
+            # Make sure within range
+            if point_position[0] > (len(resized) - 1) or point_position[0] < 0 or point_position[1] > (len(resized[len(resized)-1]) -1) or point_position[1] < 0:
+                continue
+
+            # Create new point
+            new_point = Point(current_point, point_position)
+
+            # Append
+            children.append(new_point)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_point.g + 1
+            child.h = ((child.position[0] - end_point.position[0]) ** 2) + ((child.position[1] - end_point.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_point in open_list:
+                if child == open_point and child.g > open_point.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+
+def main():
+
+    file = input("Plase enter image name: \n")
+    image = Image.open(file)
+
+    resized = image.resize((1000,1000))
+    resized.save(file)
+
+    data = np.asarray(resized, dtype='uint8')
+
+    start = (600,840)
+    end = (800,325)
+
+    path = astar(data,start, end)
+    print(path)
+
+    draw = ImageDraw.Draw(resized)
+    draw.line((start, end), fill=225)
+    resized.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ bfs.py
@@ -0,0 +1,92 @@
+from PIL import Image, ImageDraw
+from numpy import asarray
+import numpy as np
+import math
+
+def getadjacent(n):
+    x , y = n
+    return [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
+
+def evaluate(ne,end):
+    min = 0
+    pointis = ne[0]
+    for p in ne:
+        # d = int(math.sqrt(((p[0]-end[0])**2)+((p[1]-end[1])**2)))
+        d = abs(p[0]-end[0]) + abs(p[1]-end[1])
+        # print(d)
+        if d < min:
+            min = d
+            pointis = p
+            # print(pointis)
+    return pointis
+
+def bfs_shortest_path(data, start, end):
+    # keep track of explored nodes
+    explored = []
+    # keep track of all the paths to be checked
+    queue = [[start]]
+    # print(queue)
+
+    # return path if start is goal
+    if start == end:
+        return '0'
+
+    # keeps looping until all possible paths have been checked
+    while queue:
+        # pop the first path from the queue
+        # print(queue)
+        path = queue.pop(0)
+        # print(path)
+        # get the last point from the path
+        point = path[-1]
+        # point = path[0]
+        # print(point)
+        if point not in explored:
+            neighbours = getadjacent(point)
+            # print(neighbours)
+            # go through all neighbour points, construct a new path and
+            # push it into the queue
+            for neighbour in neighbours:
+                new_path = list(path)
+                # print(new_path)
+                new_path.append(neighbour)
+                # print(new_path)
+                queue.append(new_path)
+                # print(queue)
+                # return path if neighbour is goal
+                if neighbour == end:
+                    # print(new_path)
+                    return new_path
+
+            # mark point as explored
+            explored.append(point)
+            # print(explored)
+
+    # in case there's no path between the 2 points
+    return "So sorry, but a connecting path doesn't exist :("
+
+
+def main():
+
+    file = input("Plase enter image name: \n")
+    image = Image.open(file)
+
+    resized = image.resize((1000,1000))
+    resized.save(file)
+
+    data = np.asarray(resized, dtype='uint8')
+
+    tdata = data.transpose()
+
+    start = (20,310)
+    end = (600,700)
+
+    path = bfs_shortest_path(tdata,start, end)
+    print(path)
+
+    draw = ImageDraw.Draw(resized)
+    draw.line((start, end), fill=128)
+    resized.show()
+
+if __name__ == '__main__':
+    main()
